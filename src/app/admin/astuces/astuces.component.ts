36dad01922ca22db840e6e942ab038791484ef36
Omar-ciber <omarsow388@gmail.com>
2024-02-12 18:03:28 +0000

    lundi 2

--- src/app/admin/astuces/astuces.component.ts
+++ src/app/admin/astuces/astuces.component.ts
@@ -71,9 +71,9 @@ export class AstucesComponent {
     setTimeout(() => {
       this.dataSource.paginator = this.paginator;
     });
+    this.listeArticles();
     this.titre = this.contenu;
     this.user();
-    this.listeArticles();
   }
   // Issue des service
 
@@ -97,12 +97,20 @@ export class AstucesComponent {
     this.art.getArticles().subscribe(
       resoponse => {
         this.articleSliste = resoponse;
+        // console.log(resoponse);
         this.setPage(1);
       }
     )
   }
 
   ajouterArticle() {
+
+    let t = this.contenu.indexOf('>');
+    let l = this.contenu.substring(t).indexOf('<')
+    var tempDiv = document.createElement('div');
+    tempDiv.innerHTML = this.contenu.substring(t + 1, l + 2);
+
+    let tm = this.contenu.search(/<img src="/);
     const nA = new FormData();
     nA.append('titre', this.titre);
     nA.append('image', this.image as Blob);
@@ -110,26 +118,22 @@ export class AstucesComponent {
 
     this.art.createArticle(nA).subscribe(
       response => {
-        // this.setPage(this.pager.pages.length);
-        // this.listeArticles();
-        Swal.fire({
-          title: 'Success',
-          text: response.message,
-          icon: 'success'
-        })
+        this.setPage(this.pager.pages)
+        // Swal.fire({
+        //   title: 'Success',
+        //   text: response.message,
+        //   icon: 'success'
+        // })
         this.contenu = '';
-        console.log("Success", response);
-
       },
       error => {
+        alert(this.contenu.substring(tm + 10, this.contenu.substring(tm + 10).indexOf('"') + tm + 10));
         Swal.fire({
           title: 'error',
           text: error.error.error,
           icon: 'error'
         })
-        console.log('Erreur ', error);
-        this.listeArticles();
-        this.contenu = ''
+        l
       }
     )
   }
@@ -247,7 +251,7 @@ export class AstucesComponent {
     }
     )
   }
-
+  
 }
 export interface Astuces {
   id: number,
